763a2c839abf0c494bd9284008fad05fe6200607
CertAndKey <38504609+CertAndKey@users.noreply.github.com>
2022-04-27 15:56:18 +0000

    Update and rename brute.py to Brute-ForceAPI-BasicAuth.py

--- Brute-ForceAPI-BasicAuth.py
+++ Brute-ForceAPI-BasicAuth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+#Bruteforcing API with basic authentication
+#this file takes 1 arg which is a list of API endpoints
+#this file prints the status code of the get requests
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests.auth import HTTPBasicAuth
@@ -8,7 +10,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 api_file = "api.txt"
 
-url = "http://192.168.1.30"
+url = "http://<ip_address>"
 
 
 
@@ -19,6 +21,6 @@ for api in file:
     api = api.strip()
     url_full = url + api
     print(url_full)
-    r = requests.get(url_full, auth=HTTPBasicAuth('admin','password1'))
+    r = requests.get(url_full, auth=HTTPBasicAuth('<username>','<password>'))
     print(r.status_code)
 
