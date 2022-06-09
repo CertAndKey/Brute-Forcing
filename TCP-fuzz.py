e9fac9097d20b7f0756abdc395c648fa6c88fef2
CertAndKey <38504609+CertAndKey@users.noreply.github.com>
2022-06-09 13:59:20 -0400

    Update TCP-fuzz.py

--- TCP-fuzz.py
+++ TCP-fuzz.py
@@ -1,23 +1,26 @@
 import socket
 from socket import error as SocketError
 
-TCP_IP = "192.168.200.141"
-TCP_PORT = 9876
+TCP_IP = "<IP>"
+TCP_PORT = <PORT>
 
 print("TCP target IP: %s" % TCP_IP)
 print("TCP target port: %s" % TCP_PORT)
 
+#LIST OF STRINGS TO SEND TO SERVER
 input_file = open('list.txt', 'r')
 
 for line in input_file:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((TCP_IP, TCP_PORT))
+    #CLIENT DISCONNECTS IF NO RESPONSE FOR 1 SECOND
     sock.settimeout(1.0)
     try:
         sock.sendall(str.encode(line))
         data = sock.recv(1024)
         print(line)
         print(data)
+    #CATCHES SOCKET DISCONNECTION ERROR
     except SocketError as e:
         print(e)
         pass
